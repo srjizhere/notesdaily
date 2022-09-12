0fadd58bc04e92818ebbdda9f6b326435fa6b6c5
srjizhere <bhiapatil@gmail.com>
2022-09-13 00:39:56 +0530

    notes re

---
+++ groc/scripts/checkout.js
@@ -0,0 +1,8 @@
+/*
+Alerts 
+0 seconds : Your order is accepted 
+3 seconds : Your order is being Packed 
+8 seconds : Your order is in transit  
+10 seconds : Your order is out for delivery 
+12 seconds : Order delivered
+*/
